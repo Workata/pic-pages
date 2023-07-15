3427ea8a274cf68ef972d2fceae2c1d620b3d6d5
jakub tol <jakubtolsciuk@gmail.com>
2023-07-15 13:05:24 +0200

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// * navigation
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+// * pages
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn
-        </a>
-      </header>
-    </div>
+    // verify basename and homepage after deploy
+    <Router basename="/pic-pages">  
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+      
+      </Routes>
+
+    </Router>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  Box,
+  Typography,
+  Button
+} from "@mui/material";
+
+
+export default function Home() {
+
+  return (
+    <>
+      <Box>
+        Dupa
+      </Box>
+    </>
+  );
+}
