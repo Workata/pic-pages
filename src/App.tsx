30347207bec492f05e9ee81aee83397a66fddf80
jakub tol <jakubtolsciuk@gmail.com>
2023-07-22 01:34:49 +0200

    Add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,24 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Album from "./pages/Album";
 
+import AppWrapper from "./wrappers/AppWrapper";
+
 function App() {
   return (
     // TODO verify basename and homepage setting after deploy
     <Router basename="/pic-pages">  
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/album" element={<Album />} /> */}
-        <Route path="/album/:folderId" element={<Album />} />
+      <AppWrapper>
 
-      
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/album" element={<Album />} /> */}
+          <Route path="/album/:folderId" element={<Album />} />
+
+        
+        </Routes>
+    
+      </AppWrapper>
 
     </Router>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import {
   Box,
-  Typography,
-  Button
+    Typography
+
 } from "@mui/material";
 import { Link } from "react-router-dom";
+
+
 
 
 export default function Home() {
 
   return (
     <>
-      <Box>
-        <Link to={`/album/${process.env.REACT_APP_ROOT_FOLDER_ID}`}>Album</Link>
-      </Box>
+        Home page    Home page   Home page
+      Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
+          eget nulla facilisi etiam dignissim diam. Home page
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
+          eget nulla facilisi etiam dignissim diam. 
+        </Typography>
     </>
   );
 }
