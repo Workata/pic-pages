ad674bd9d89252df9da1ece2fa0f998141601134
jakub tol <jakubtolsciuk@gmail.com>
2023-12-03 13:57:53 +0100

    Integrate categories

--- src/components/modals/SelectCategory.tsx
+++ src/components/modals/SelectCategory.tsx
@@ -15,7 +15,7 @@ import { fetchCategories } from "../../clients/fetchCategories";
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import { getImageData, postImageData } from "../../services/images";
+import { getImageData, postImageData, patchImageCategories } from "../../services/images";
 import { fetchImageData } from "../../clients/fetchImageData";
 
 
@@ -28,6 +28,28 @@ export default function SelectCategoryModal(props: any) {
     setImageData(undefined);
     // setCategories([]);
   };
+
+  const updateCategories = (categoryName: string) => {
+    console.log("Update category")
+    let updatedImageData = imageData;
+    let updatedCategories = imageData!.categories.map((category) => category.name);
+    if (updatedCategories?.includes(categoryName)) {
+      // * uncheck category
+      const index = updatedCategories.indexOf(categoryName);
+      updatedCategories.splice(index, 1); // 2nd parameter means remove one item only
+      updatedImageData!.categories = imageData!.categories.filter((category) => category.name !== categoryName);
+      setImageData(updatedImageData);
+    } else {
+      // * check category
+      updatedCategories.push(categoryName);
+      updatedImageData!.categories = imageData!.categories.concat([new Category({'name': categoryName})]);
+      setImageData(updatedImageData);
+    }
+    patchImageCategories(props.imgId, updatedCategories,
+      (res: any) => {console.log(res)}, (err: any) => {console.log(err)}
+    )
+    // fetchImageData(props.imgId, setImageData);
+  }
 
 
   useEffect(() => {
@@ -65,6 +87,7 @@ export default function SelectCategoryModal(props: any) {
             control={imageData?.categories.find(e => e.name === category.name)? <Checkbox defaultChecked/> : <Checkbox/>} 
             label={category.name} 
             key={category.name}
+            onChange={() => updateCategories(category.name)}
            />
           )}
         </FormGroup>
--- src/services/images.ts
+++ src/services/images.ts
@@ -15,6 +15,20 @@ export const getImageData = async (img_id: any, callback: any, errorcallback: an
 }
 
 
+export const patchImageCategories = async (img_id: any, data: Array<string>, callback: any, errorcallback: any): Promise<any> => {
+  axios.patch(`/api/v1/images/${img_id}/categories`, data=data).then( res => {
+    if(callback != null){
+      callback(res);
+    }
+  }
+  ).catch( err => {
+    if(errorcallback != null){
+      errorcallback(err);
+   }
+  })
+}
+
+
 export const postImageData = async (data: any, callback: any, errorcallback: any): Promise<any> => {
   axios.post('/api/v1/images', data=data
   ).then( res => {
